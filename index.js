469796d2cbc218a9f1c45c7d22035fef139162c9
Marcin Wisniowski <shfx@shfx.pl>
2019-09-24 14:04:31 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const fetch = require("node-fetch");
+const ora = require("ora");
+const chalk = require("chalk");
+
+const spinner = ora().start("Checking...");
+
+let fails = 0;
+
+const timeout = (message, time) =>
+  new Promise((_, reject) => {
+    setTimeout(() => reject(message), time);
+  });
+
+const verify = async (section, item, key) => {
+  try {
+    spinner.start(`Checking ${chalk.green(item.title)}`);
+    return await Promise.race([fetch(item[key]), timeout(`Timeout`, 10000)]);
+  } catch (e) {
+    fails += 1;
+    spinner.fail(
+      [
+        `Section ${section.title} - ${item.title} - ${chalk.red(key)} - (${
+          item[key]
+        })`,
+        `    - ${e}`,
+      ].join("\n"),
+    );
+  }
+};
+
+const checkItem = (section) => async (item) => {
+  spinner.indent = 2;
+
+  let store = await verify(section, item, "url_store");
+  let image = await verify(section, item, "url_image");
+
+  if (store && image) {
+    spinner.start();
+    spinner.succeed(item.title);
+  }
+};
+
+const checkSection = async (section) =>
+  Promise.all(section.items.map(checkItem(section)));
+
+const main = async (args) => {
+  const res = await fetch(args);
+
+  const data = await res.json();
+
+  for (const section of data.sections) {
+    await checkSection(section);
+  }
+  spinner.stop();
+  process.exit(fails > 0 ? 1 : 0);
+};
+
+process.on("SIGINT", function() {
+  process.exit(1);
+});
+
+main(process.argv[2]);
